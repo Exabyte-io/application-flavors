40465b35507150a129b8d221078d1e76290ffb20
Alexander Zech <alexzech@gmx.de>
2022-12-19 14:15:53 -0800

    refactor: use deepclone from code.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -12,14 +12,6 @@ export const makeObjectsFromContextProviderNames = (asset) => {
 };
 
 /**
- * Creates deep clone of the object.
- * @param obj {Object}
- */
-export function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
-
-/**
  * Load an asset file for the given application, of the given name.
  * @param applicationPath {str} The directory path in the /assets directory of this package, for this application
  * @param filename {str} The name of the file in the applicationPath directory to load and return

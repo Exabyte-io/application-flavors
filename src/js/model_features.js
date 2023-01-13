70a9c5cf9bece4f812e1358f8b275dec31cb5140
Alexander Zech <alexzech@gmx.de>
2023-01-13 08:42:44 -0800

    refactor: move filterTree to mode.js

--- src/js/model_features.js
+++ src/js/model_features.js
@@ -1,50 +1,6 @@
-import lodash from "lodash";
+import { filterTree } from "@exabyte-io/mode.js/dist/tree";
 
 import modelFeatures from "../../model_feature_data";
-import { recursiveMerge } from "./utils";
-
-/**
- * Filter nodes from the model tree based on path and modify node data.
- * A node of the model tree is defined in mode.js and has a form such as the following:
- * ```
- * {
- *     path: "/dft/gga",
- *     parentPath: "/dft",
- *     data: {
- *         subtype: {
- *             slug: "gga",
- *             name: "Generalized Gradient Approximation",
- *         },
- *     },
- *     children: [
- *         // array of child nodes
- *     ],
- * }
- * ```
- * @param {Array} nodes - Array of nodes to be filtered.
- * @param {string[]} paths - Array of node paths
- * @param {Object[]} pathData - Array of objects containing path and node data.
- * @returns {Object[]} - The filtered tree (with possibly modified data).
- */
-export function filterTree(nodes, paths, pathData = null) {
-    return nodes.reduce((accumulator, node) => {
-        if (paths.includes(node.path) || node.path === "") {
-            let modified = {};
-            let data = pathData && pathData.find((item) => item.path === node.path)?.data;
-            if (data) {
-                data = recursiveMerge(node.data, data);
-                modified = { ...node, data };
-            }
-            if (node.children?.length) {
-                const children = filterTree(node.children, paths, pathData);
-                if (children.length) modified = { ...node, ...modified, children };
-            }
-            // eslint-disable-next-line no-unused-expressions
-            lodash.isEmpty(modified) ? accumulator.push(node) : accumulator.push(modified);
-        }
-        return accumulator;
-    }, []);
-}
 
 /**
  * Selects a subset of the model tree for a given application

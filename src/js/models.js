a68f1b2d65b1772732ec0df731522a51de0265c8
Alexander Zech <alexzech@gmx.de>
2023-01-19 15:00:34 -0800

    chore: allow filter by name, version, and build only

--- src/js/models.js
+++ src/js/models.js
@@ -1,4 +1,5 @@
 import { filterTree } from "@exabyte-io/mode.js/dist/tree";
+import { uniqBy } from "lodash/array";
 
 import availableModels from "../../model_list";
 
@@ -17,7 +18,17 @@ export function getModelTreeByApplication({
     executable,
     build = "Default",
 }) {
-    const modelList = availableModels[appName][version][executable][build];
+    let modelList;
+    if (!executable) {
+        modelList = uniqBy(
+            Object.keys(availableModels[appName][version]).flatMap(
+                (exec) => availableModels[appName][version][exec][build],
+            ),
+            "path",
+        );
+    } else {
+        modelList = availableModels[appName][version][executable][build];
+    }
     const nodePaths = modelList.map((item) => item.path);
     const pathData = modelList.filter((item) => "data" in item);
     const nodes = Array.isArray(tree) ? tree : [tree];

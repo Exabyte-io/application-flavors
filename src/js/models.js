27f7100528a0db61082af62ad877dbd529e7c401
Alexander Zech <alexzech@gmx.de>
2023-03-01 19:27:50 -0800

    chore: get filter list with partial input

--- src/js/models.js
+++ src/js/models.js
@@ -1,3 +1,4 @@
+import { mergeTerminalNodes } from "@exabyte-io/code.js/dist/utils/object";
 import { filterTree } from "@exabyte-io/mode.js/dist/tree";
 
 import availableModels from "../../model_list";
@@ -22,4 +23,29 @@ export function getModelTreeByApplication({
     const pathData = modelList.filter((item) => "data" in item);
     const nodes = Array.isArray(tree) ? tree : [tree];
     return filterTree(nodes, nodePaths, pathData);
+}
+
+export function getFilterList({
+    filterObj,
+    appName = "",
+    version = "",
+    build = "",
+    executable = "",
+    flavor = "",
+}) {
+    let filterList;
+    if (!appName) {
+        filterList = mergeTerminalNodes(filterObj, true);
+    } else if (!version) {
+        filterList = mergeTerminalNodes(filterObj[appName], true);
+    } else if (!build) {
+        filterList = mergeTerminalNodes(filterObj[appName][version], true);
+    } else if (!executable) {
+        filterList = mergeTerminalNodes(filterObj[appName][version][build], true);
+    } else if (!flavor) {
+        filterList = mergeTerminalNodes(filterObj[appName][version][build][executable], true);
+    } else {
+        filterList = filterObj[appName][version][build][executable][flavor];
+    }
+    return filterList;
 }

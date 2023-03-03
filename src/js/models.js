adcf6fb1f8b72c89fe9026d28602ee3ff6ccb2dd
Alexander Zech <alexzech@gmx.de>
2023-03-02 16:40:04 -0800

    chore: add general function for filtering entity paths

--- src/js/models.js
+++ src/js/models.js
@@ -68,3 +68,26 @@ export function getFilterLists({
 
     return [extractUniqueBy("path"), extractUniqueBy("regex")];
 }
+
+/**
+ * Filter list of entity paths for a given application
+ * @param {{appName: string, version: string, build: string, executable: string, flavor: string}} applicationData
+ * @param {Array<Object>} entityPathCollection - Array of objects defining entity path
+ * @param {Object} entityFilterObj - filter object with nesting [appName][version][build][executable][flavor]
+ * @return {Array} - entity path objects filtered by application
+ */
+export function filterEntityPaths({ applicationData, entityPathCollection, entityFilterObj }) {
+    const [pathList, regexList] = getFilterLists({
+        filterObj: entityFilterObj,
+        ...applicationData,
+    });
+
+    const filtered = regexList
+        .flatMap((r) => {
+            const regex = new RegExp(r.regex);
+            return entityPathCollection.filter((o) => regex.test(o.path));
+        })
+        .concat(lodash.intersectionBy(entityPathCollection, pathList, "path"));
+
+    return lodash.uniqBy(filtered, "path");
+}

ec013a4a06257eed0a9e4045989aafd6656a2821
Alexander Zech <alexzech@gmx.de>
2023-03-02 19:48:21 -0800

    chore: fix function parameters + asset cleanup

--- src/js/models.js
+++ src/js/models.js
@@ -3,12 +3,12 @@ import { filterEntityPaths } from "./filters";
 
 /**
  * Filters list of models for a given application
- * @param {Object|Array} tree - Tree object or array of nodes
- * @param {string} appName
- * @param {string} version
- * @param {string} build
- * @param {string} executable
- * @param {string} flavor
+ * @param {Array<Object>} modelPaths - List of objects containing model path
+ * @param {string} appName - application name
+ * @param {string} version - application version
+ * @param {string} build - application build
+ * @param {string} executable - executable name
+ * @param {string} flavor - flavor name
  * @returns {Array<Object>} - filtered list of model paths for given application data
  */
 export function filterModelsByApplication({
@@ -20,11 +20,7 @@ export function filterModelsByApplication({
     flavor,
 }) {
     return filterEntityPaths({
-        appName,
-        version,
-        build,
-        executable,
-        flavor,
+        applicationData: { appName, version, build, executable, flavor },
         entityFilterObj: availableModels,
         entityPathCollection: modelPaths,
     });

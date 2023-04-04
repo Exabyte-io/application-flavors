18bdcc17cf59cd4ec3b332454573dd371ed6491a
Alexander Zech <alexzech@gmx.de>
2023-04-04 11:40:41 -0700

    feat: add espresso version configs supported on CLI

--- src/js/espresso/data.js
+++ src/js/espresso/data.js
@@ -6,19 +6,112 @@ export default {
     versions: [
         {
             version: "5.2.1",
+            isDefault: true,
             hasAdvancedComputeOptions: true,
         },
         {
-            isDefault: true,
+            version: "5.2.1",
+            isDefault: false,
+            build: "GNU",
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "5.2.1",
+            isDefault: false,
+            build: "Intel",
+            hasAdvancedComputeOptions: true,
+        },
+        {
             version: "5.4.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "5.4.0",
+            isDefault: false,
+            build: "GNU",
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "5.4.0",
+            isDefault: false,
+            build: "Intel",
             hasAdvancedComputeOptions: true,
         },
         {
             version: "6.0.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.0.0",
+            isDefault: false,
+            build: "GNU",
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.0.0",
+            isDefault: false,
+            build: "Intel",
             hasAdvancedComputeOptions: true,
         },
         {
             version: "6.3",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.3",
+            isDefault: false,
+            build: "GNU",
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.3",
+            isDefault: false,
+            build: "Intel",
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.4.1",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.5.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.6.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.7.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "6.8.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "7.0",
+            isDefault: true,
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "7.0",
+            isDefault: false,
+            build: "GNU",
+            hasAdvancedComputeOptions: true,
+        },
+        {
+            version: "7.0",
+            isDefault: false,
+            build: "Intel",
             hasAdvancedComputeOptions: true,
         },
     ],

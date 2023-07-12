e8965f7405d46bcc209f1fc42d482fd0b098f9a9
Alexander Zech <alexzech@gmx.de>
2023-07-12 13:48:46 -0700

    chore: mock filter_list to avoid import

--- src/js/methods.js
+++ src/js/methods.js
@@ -1,7 +1,29 @@
 import { filterEntityList } from "@exabyte-io/code.js/dist/utils";
 
-import { methods as applicationMethodMap } from "../../filter_trees";
 import { getFilterObjects } from "./models";
+
+// TODO: reactivate once epic/SOF-6009 has been merged
+// import { methods as applicationMethodMap } from "../../filter_trees";
+
+// placeholder for applicationMethodMap
+const applicationMethodMap = {
+    espresso: {
+        "5.2.1": {
+            Default: {
+                "pw.x": {
+                    pw_scf: [
+                        { path: "/qm/wf/undefined/pw/undefined" },
+                        { regex: "/qm/wf/undefined/psp/.*" },
+                        { regex: "/qm/wf/undefined/smearing/.*" },
+                        { regex: "/qm/wf/undefined/tetrahedron/.*" },
+                        { path: "/opt/diff/ordern/cg/undefined" },
+                        { path: "/linalg/diag/undefined/davidson/undefined" },
+                    ],
+                },
+            },
+        },
+    },
+};
 
 export function filterMethodsByApplicationParameters({
     methodList,

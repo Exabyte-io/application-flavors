138b6dedfd0b268eab7b7406d2daf0f93ba6724a
Alexander Zech <alexzech@gmx.de>
2023-08-04 17:09:57 -0700

    chore: update built JS files

--- src/js/data/application_data.js
+++ src/js/data/application_data.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0550452714e4a9742a34b868b97f953c95313ae3b022b604780baf612c8e8d10
-size 5323
+oid sha256:448322dc7e13f72a8a688bba946aa3336caa674767577fbd4f57cb97c8cfaa8e
+size 5627

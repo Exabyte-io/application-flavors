7da589b4102454f94d9388399ec6aa7225e8b2df
Alexander Zech <alexzech@gmx.de>
2023-10-27 14:56:16 -0700

    refactor: use asset functions from code.js - part2

--- build_templates.js
+++ build_templates.js
@@ -3,35 +3,21 @@
  * at build time and writes them out to a single templates.js file for
  * downstream consumption to avoid FS calls in the browser.
  */
-const fs = require("fs");
-const yaml = require("js-yaml");
 const utils = require("@exabyte-io/code.js/dist/utils");
 
-function buildAsset({ assetPath, targetPath, dataKey = "" }) {
-    const fileContent = fs.readFileSync(assetPath);
-    const obj = yaml.load(fileContent, { schema: utils.JsYamlAllSchemas });
-    const ignore = "/* eslint-disable */\n";
-    fs.writeFileSync(
-        targetPath,
-        ignore + `module.exports = {${dataKey}: ` + JSON.stringify(obj) + "}\n",
-        "utf8",
-    );
-    console.log(`Written asset "${assetPath}" to "${targetPath}"`);
-}
-
-buildAsset({
+utils.buildJsAssetFromYaml({
     assetPath: "./templates/templates.yml",
     targetPath: "./src/js/data/templates.js",
     dataKey: "allTemplates",
 });
 
-buildAsset({
+utils.buildJsAssetFromYaml({
     assetPath: "./applications/application_data.yml",
     targetPath: "./src/js/data/application_data.js",
     dataKey: "applicationData",
 });
 
-buildAsset({
+utils.buildJsAssetFromYaml({
     assetPath: "./executables/tree.yml",
     targetPath: "./src/js/data/tree.js",
     dataKey: "applicationTree",

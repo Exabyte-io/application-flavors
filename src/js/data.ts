513e4daf188c1ef1fcfe8195e2c90e289f616296
Sean Warren <seankwarren722@gmail.com>
2024-02-09 14:15:05 -0800

    fix: app tree types

--- src/js/data.ts
+++ src/js/data.ts
@@ -7,9 +7,10 @@ import { applicationData as APP_DATA } from "./data/application_data";
  * @param appName {str}
  * @returns {object}
  */
-export function getAppData(appName: AllowedApplications): ApplicationData {
-    if (!(appName in APP_DATA)) {
+export function getAppData(appName: AllowedApplications) {
+    const app_data = APP_DATA as {[key in AllowedApplications]: ApplicationData};
+    if (!(appName in app_data)) {
         throw new Error(`${appName} is not a known application with data.`);
     }
-    return APP_DATA[appName] as ApplicationData;
+    return app_data[appName];
 }

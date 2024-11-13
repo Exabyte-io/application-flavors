701654bc1a341eddffef0d7693535cf68a727b74
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-11-13 15:30:10 +0200

    chore: remove hyphens from raw

--- assets/shell/bash_espresso_collect_dynmat.j2.sh
+++ assets/shell/bash_espresso_collect_dynmat.j2.sh
@@ -1,5 +1,5 @@
-{%- raw -%}
+{% raw %}
 #!/bin/bash
 
 cp {{ JOB_SCRATCH_DIR }}/outdir/_ph0/__prefix__.phsave/dynmat* {{ JOB_WORK_DIR }}/../outdir/_ph0/__prefix__.phsave
-{%- endraw -%}
+{% endraw %}
--- assets/shell/bash_espresso_link_outdir_save.j2.sh
+++ assets/shell/bash_espresso_link_outdir_save.j2.sh
@@ -1,7 +1,7 @@
-{%- raw -%}
+{% raw %}
 #!/bin/bash
 
 mkdir -p {{ JOB_SCRATCH_DIR }}/outdir/_ph0
 cd {{ JOB_SCRATCH_DIR }}/outdir
 cp -r {{ JOB_WORK_DIR }}/../outdir/__prefix__.* .
-{%- endraw -%}
+{% endraw %}
